883912c911069525b22106c5529091d279ef4ae3
Dirk Christian Becker <dirk.christian.becker@gmail.com>
2023-04-07 15:51:14 +0200

    Changed mods to pub

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-mod rotating_shapes;
-mod rotator;
+pub mod rotating_shapes;
+pub mod rotator;
 
 pub mod prelude {
 	/// Interface
 	pub use crate::rotating_shapes::RotatingShapes;
+	pub use crate::rotator::Rotator;
 }
